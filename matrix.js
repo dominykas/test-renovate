569e9825ef0ce725b9218de3d28cb511aa5a6240
Dominykas Blyžė <hello@dominykas.com>
2020-10-13 23:36:26 +0300

    ci: try out a custom build matrix

--- matrix.js
+++ matrix.js
@@ -1,1 +1,15 @@
-console.log(`::set-output name=matrix::${JSON.stringify([10, 12, 14])}`);
+'use strict';
+
+const core = require('@actions/core');
+
+const matrix = [10, 12, 14];
+
+try {
+    console.log(`Min version: ${core.getInput('gte')}`)
+    console.log(`Policy: ${core.getInput('policy')}`)
+    console.log(`Matrix: ${matrix}`);
+
+    core.setOutput("matrix", matrix);
+} catch (error) {
+    core.setFailed(error.message);
+}
