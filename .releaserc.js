b6cd30c6445011930e3020da9cc2d7e1dd22103a
Dominykas Blyžė <hello@dominykas.com>
2020-11-16 14:00:55 +0200

    chore: cleanup

--- .releaserc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    plugins: [
-        '@semantic-release/commit-analyzer',
-        '@semantic-release/release-notes-generator',
-        './lib/poc-npm-set-otp.js',
-        '@semantic-release/npm',
-        '@semantic-release/github'
-    ]
-};
